8a176d5dd370988effeef831be21668ead29332e
Tiffany <tiffany.chuang2@gmail.com>
2019-05-23 23:03:59 -0400

    start over

--- archive/static/js/app.js
+++ archive/static/js/app.js
@@ -1,37 +1,18 @@
-let links = document.querySelectorAll('a');
-for (let i = 0; i < links.length; i++) {
-    links[i].addEventListener('mouseover', function() {
-        this.style.fontSize = '120%';
-    });
-    links[i].addEventListener('mouseout', function() {
-        this.style.fontSize = '22px';
-    });
-}
-
-//Geography animation: https://www.w3schools.com/howto/howto_js_animate.asp
-function myMove() {
-  var elem = document.getElementById("myAnimation");
-  var pos = 1;
-  var id = setInterval(frame, 10);
-  function frame() {
-    if (pos == 350) {
-      clearInterval(id);
-    } else {
-      pos++;
-      elem.style.bottom = pos + 'px';
-      elem.style.left = pos + 'px';
-    }
-  }
-}
-
-
-// //copyright hover
-// let copyright = document.querySelectorAll('.copyright');
-// for(let i=0; i<copyright.length; i++) {
-//     copyright[i].addEventListener('mouseover', function() {
-//         this.innerText = 'Created By Aaron Barclay, Tiffany Chuang, Gene Chung, Leslie Geissler, Jaimie Obidike, Jesse Snyder, and Mary Ziegler';
+// let links = document.querySelectorAll('a');
+// for (let i = 0; i < links.length; i++) {
+//     links[i].addEventListener('mouseover', function() {
+//         this.style.fontSize = '120%';
 //     });
-//     copyright[i].addEventListener('mouseout', function() {
-//         this.innerText = 'Copyright © 2017';
+//     links[i].addEventListener('mouseout', function() {
+//         this.style.fontSize = '22px';
 //     });
 // }
+
+// click to download resume
+// var download = document.getElementById('p3');
+// download.onclick = function () {
+//     window.open('./Resume.pdf');
+// }
+
+
+
